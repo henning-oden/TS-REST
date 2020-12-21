9cb6879fcb82869384a49e6f57bc4b0b1947cab0
Henning Odén <henning.oden@outlook.com>
2020-12-21 15:41:14 +0100

    Compiled ts code and fixed bug

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-var express = require('express');
-var app = express();
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const todos_1 = __importDefault(require("./routes/todos"));
+const app = express_1.default();
+app.use(body_parser_1.default.json());
+app.use(todos_1.default);
 app.listen(3000);
--- routes/todos.js
+++ routes/todos.js
@@ -1,25 +1,19 @@
-import { Router } from 'express';
-
-import { Todo } from '../models/todo';
-
-let todos: Todo[] = [];
-
-const router = Router();
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+let todos = [];
+const router = express_1.Router();
 router.get('/', (req, res, next) => {
-    res.status(200).json({ todos: todos })
+    res.status(200).json({ todos: todos });
 });
-
 router.post('/todo', (req, res, next) => {
-    const newTodo: Todo = {
+    const newTodo = {
         id: new Date().toISOString(),
         text: req.body.text
     };
-
     todos.push(newTodo);
     res.status(201).json({ message: 'Added todo', todo: newTodo, todos: todos });
 });
-
 router.put('/todo/:todoId', (req, res, next) => {
     const tid = req.params.todoId;
     const todoIndex = todos.findIndex(todoItem => todoItem.id === tid);
@@ -29,10 +23,8 @@ router.put('/todo/:todoId', (req, res, next) => {
     }
     res.status(400).json({ message: 'Could not find todo for this id.' });
 });
-
 router.delete('/todo/:todoId', (req, res, next) => {
     todos = todos.filter(todoItem => todoItem.id != req.params.todoId);
     res.status(200).json({ message: 'Deleted todo', todos: todos });
 });
-
-export default router;
+exports.default = router;
